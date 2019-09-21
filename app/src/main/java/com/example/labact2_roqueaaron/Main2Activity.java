4ca0a687316f2f848778af745291457f791d00cf
Aaron <aaronjohn.roque.iics@ust.edu.ph>
2019-09-21 13:56:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/labact2_roqueaaron/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.example.labact2_roqueaaron;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Main2Activity extends AppCompatActivity {
+
+    EditText utCheckCourse, 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
